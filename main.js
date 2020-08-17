c8077b5d0cdf2dd9eec5f64feb5e3c9048ff8be9
MrRiaz <monsurrahmanriaz@gmail.com>
2020-08-18 00:08:08 +0600

    done

--- main.js
+++ main.js
@@ -38,6 +38,28 @@ function showData(data){
     }
     </ul>
     `;
+};
+
+result.addEventListener('click', e => {
+    const clickedEl = e.target;
+
+    if (clickedEl.tagName === 'BUTTON') {
+        const artist = clickedEl.getAttribute('data-artist');
+        const songTitle = clickedEl.getAttribute('data-songtitle');
+
+        getLyrics(artist, songTitle);
+    }
+});
+
+
+async function getLyrics(artist, songTitle) {
+    const res = await fetch(`${apiURL}/v1/${artist}/${songTitle}`);
+    const data = await res.json();
+
+    const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>');
+
+    result.innerHTML = `<h2><strong>${artist}</strong> - ${songTitle}</h2>
+    <span>${lyrics}</span>`;
 }
 
 
